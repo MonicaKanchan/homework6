c5770f629a79244335aa934a8d3e1cf4fe5ce7af
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-11-19 16:31:09 -0500

    second commit

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ try
 self::$db = new PDO( 'mysql:host=sql1.njit.edu; dbname=mk758',
 'mk758','MxLEzvEVX');
 self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "<b>Connection Successful</b>"."<br>";
 }
 catch(PDOException $e)
 {
@@ -31,8 +32,51 @@ return self::$db;
 }
 }
 $db = dbConn::getConnection();
-echo "<b>Connection Successful</b>"."<br>";
+//echo "<b>Connection Successful</b>"."<br>";
+
+class Collection
+{
+static public function create()
+{
+$model=new static::$modelName;
+return$model;
+}
+static public function findAll()
+{
+$db=dbConn::getConnection();
+$tableName=get_called_class();
+$sql='SELECT * FROM' .$tableName;
+$statement = $db->prepare($sql);
+$statement->execute();
+$class = static::$modelName;
+$statement->setFetchMode(PDO::FETCH_CLASS,$class);
+$recordsSet= $statement->fetchAll();
+return $recordsSet;
+}
+
+static public function findOne($id)
+{
+$db=dbConn::getConnection();
+$tableName=get_called_class();
+$sql='SELECT * FROM '.$tableName. 'WHERE id=' .$id;
+$statement=$db->prepare($sql);
+$statement->execute();
+$class=static::$modelName;
+$statement->setFetchMode(PDO::FETCH_CLASS, $class);
+$recordsSet= $statement->fetchAll();
+return $recordsSet[0];
+}
+}
+
+class accounts extends Collection
+{
+protected static $modelName = 'account';
+}
+
+class todos extends Collection
+{
+protected static $modelName = 'todo';
+}
+
 
 ?>
-
-
